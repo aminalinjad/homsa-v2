48c1b00e806c22c166e3e5a24be7cadff80fb40a
s.nasri <s.nasri@ernyka.com>
2021-09-07 14:42:37 +0430

    calendar is in progressing

---
+++ components/app/AppCalendar.vue
@@ -0,0 +1,119 @@
+<template>
+    <v-card 
+    width="864"
+    min-height="370"
+    >
+    <v-row>
+        <v-col>
+            <v-row>
+                <v-col>
+                    header
+
+                </v-col>
+                
+            </v-row>
+            <v-row>
+                <v-col>
+                    <v-row >
+                        <v-col>
+                            week head
+                        </v-col>
+                    </v-row>
+                    <v-row>
+                        <v-col>
+                            week
+                        </v-col>
+                    </v-row>
+                </v-col>
+            </v-row>
+        </v-col>
+        <v-col>
+            h
+        </v-col>
+
+    </v-row>
+    <v-row>
+        <v-col>
+            v
+
+        </v-col>
+
+    </v-row>
+    </v-card>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            calender:[
+                {
+                    month: 'فروردین',
+                    year: '۱۴۰۰',
+                    weekHead: [
+                        'ش',
+                        'ی',
+                        'د',
+                        'س',
+                        'چ',
+                        'پ',
+                        'ج',
+                    ],
+                    weeks:[
+                        [
+                            '۱',
+                            '۲',
+                            '۳',
+                            '۴',
+                            '۵',
+                            '۶',
+                            '۷',
+                        ],
+                        [
+                            '۸',
+                            '۹',
+                            '۱۰',
+                            '۱۱',
+                            '۱۲',
+                            '۱۳',
+                            '۱۴',
+                        ],
+                        [
+                            '۱۵',
+                            '۱۶',
+                            '۱۷',
+                            '۱۸',
+                            '۱۹',
+                            '۲۰',
+                            '۲۱',
+                        ],
+                        [
+                            '۲۲',
+                            '۲۳',
+                            '۲۴',
+                            '۲۵',
+                            '۲۶',
+                            '۲۷',
+                            '۲۸',
+                        ],
+                        [
+                            '۲۹',
+                            '۳۰',
+                            '۳۱',
+                            '',
+                            '',
+                            '',
+                            '',
+                        ]
+                    ]
+                }
+            ]
+        }
+    }
+    
+}
+</script>
+
+<style lang="scss">
+
+</style>
--- pages/_id/index.vue
+++ pages/_id/index.vue
@@ -1,9 +1,10 @@
 <template>
   <div>
-    <h1>new id page</h1>
+    <!-- <h1>new id page</h1> -->
     <v-btn color="primary" @click="useService"
       >get currency from service file</v-btn
     >
+    <AppCalendar />
   </div>
 </template>
 <script>
