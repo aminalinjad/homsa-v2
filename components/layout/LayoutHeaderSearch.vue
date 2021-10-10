461ef80c91a23c666c19d2907b5410c303518d18
amin <amin.anzd@gmail.com>
2021-10-10 18:38:12 +0330

    add guest to search header

--- components/layout/LayoutHeaderSearch.vue
+++ components/layout/LayoutHeaderSearch.vue
@@ -104,10 +104,10 @@
 
                 <div class="px-3">
                   <span
-                    v-if="searchFormValue.count"
+                    v-if="searchFormValue.guest"
                     :class="$i18n.locale === 'fa' ? 'font-FaNummedium-14' : ''"
                   >
-                    {{ searchFormValue.count }}
+                    {{ searchFormValue.guest }}
                     {{ $t("header.top.input.unit") }}
                   </span>
                   <span class="font-regular-14 secondary--text" v-else>{{
@@ -512,7 +512,7 @@ export default {
         checkIn: "12/08",
         checkOut: "12/31",
         flexibility: 1,
-        count: 1
+        guest: 1
       },
       suggestionsDefault: [],
       suggestion: true,
@@ -582,10 +582,10 @@ export default {
         data.q = this.userDestinationSearch
       }
       console.log(data)
-      this.$router.push({query: {...this.$route.query, guest: this.searchForm.guest , q: data.q}})
       this.searchFormValue.destination = this.userDestinationSearch
       this.searchFormValue.guest = this.searchForm.guest
       SearchServices.searchResults(data).then(res => {
+        this.$router.push({query: {...this.$route.query, guest: this.searchForm.guest , q: data.q}})
         this.$nuxt.$loading.finish()
         console.log(res.data)
         this.setSearchResult(res.data)
