7ef007a277bfc56bb7724a16ac4a27d0fec57ea3
amin <amin.anzd@gmail.com>
2021-11-10 11:59:53 +0330

    change the function of slug page

--- pages/_slug/slug.js
+++ pages/_slug/slug.js
@@ -1,6 +1,7 @@
 import {mapGetters, mapActions} from "vuex";
 import * as types from "@/store/types.js";
 import {SearchServices} from "@/services";
+import {indexOf} from "core-js/internals/array-includes";
 
 export default {
   layout: "search",
@@ -31,7 +32,24 @@ export default {
       value: splitSlug[1],
       type: splitSlug[0]
     }]
+
+    for (let [routeQueryKey, routeQueryValue] of Object.entries(route.query)) {
+      console.log(routeQueryKey)
+      if (routeQueryKey.match('\\[(.*?)\\]')) {
+        data[routeQueryKey.split('[')[0]] = {...data[routeQueryKey.split('[')[0]], [routeQueryKey.match('\\[(.*?)\\]')[1]]: +routeQueryValue}
+      } else {
+        if (routeQueryValue) {
+          if (routeQueryValue === 'true') {
+            data[routeQueryKey] = true
+          }else {
+            data[routeQueryKey] = +routeQueryValue
+          }
+        }
+      }
+    }
+    console.log(data)
     return SearchServices.searchResults(data).then(res => {
+      console.log(res)
       store.dispatch('modules/requestData/SET_REQUEST_DATA', data)
       store.dispatch('modules/search/SET_SEARCH_RESULTS', res.data)
       store.dispatch('modules/filters/SET_FILTERS', res.data.filters.filters)
@@ -41,12 +59,16 @@ export default {
         results: res.data.data
       }
     }).catch(err => {
+      console.log(err)
     })
   },
   computed: {
     ...mapGetters({
       getSearchResult: `modules/search/${types.search.getters.GET_SEARCH_RESULTS}`,
     }),
+  },
+  mounted() {
+    this.test()
   },
   methods: {
     ...mapActions({
@@ -55,85 +77,24 @@ export default {
     }),
 
     test() {
-      let routeQueries = this.$route.query;
-      let filterTypes = this.filterTypes;
-
-      filterTypes.forEach((filterType, filterTypeIndex) => {
-        if (filterType.type === "price_range") {
-          if (routeQueries.min_price) {
-            this.rangeSliderFrom = this.$route.query.min_price;
-            this.data.min_price = parseInt(routeQueries.min_price);
-          }
-          if (routeQueries.max_price) {
-            this.rangeSliderTo = this.$route.query.max_price;
-            this.data.max_price = parseInt(routeQueries.max_price);
-          } else if (filterType.type === "counter") {
-            // if (filterType.slug in routeQueries) {
-            //   alert(filterType.slug)
-            //   alert(routeQueries[filterType.slug])
-            // }
-            for (let [routeQueryKey, routeQueryValue] of Object.entries(
-              routeQueries
-            )) {
-              if (routeQueryKey.includes(filterType.slug)) {
-                this.data[filterType.slug] = routeQueryValue
-                  ? parseInt(routeQueryValue)
-                  : 0
-              }
-            }
-          }else if (filterTypes[filterTypeIndex].type === "list_counter") {
-            let filterListCounterItems = []
-
-            for (let [routeQueryKey, routeQueryValue] of Object.entries(
-              routeQueries
-            )) {
-              if (routeQueryKey.includes(filterType.slug)) {
-                let routeQueryId = parseInt(
-                  routeQueryKey.substring(
-                    filterType.slug.length + 1,
-                    routeQueryKey.length - 1
-                  )
-                );
-                if (routeQueryValue) {
-                  filterListCounterItems.push({
-                    id: routeQueryId,
-                    count: parseInt(routeQueryValue),
-                  });
-                }
-
-              }
-            }
-            if (filterListCounterItems.length > 0) {
-              this.data[filterType.slug] = filterListCounterItems;
-            }
-          }else if (filterTypes[filterTypeIndex].type === "switch") {
-            for (let [routeQueryKey, routeQueryValue] of Object.entries(
-              routeQueries
-            )) {
-              if (routeQueryKey.includes(filterType.slug)) {
-                this.data[filterType.slug] = routeQueryValue === "true";
-              }
-            }
-          }else if (
-            filterTypes[filterTypeIndex].type === "list" ||
-            filterTypes[filterTypeIndex].type === "list_checkbox"
-          ) {
-            let filterCheckBoxItems = [];
-            for (let [routeQueryKey, routeQueryValue] of Object.entries(
-              routeQueries
-            )) {
-              if (routeQueryKey.includes(filterType.slug)) {
-                if (routeQueryValue) {
-                  filterCheckBoxItems.push(parseInt(routeQueryValue));
-                }
-              }
-            }
-            if (filterCheckBoxItems.length > 0) {
-              this.data[filterType.slug] = filterCheckBoxItems;
+      let data = {}
+      for (let [routeQueryKey, routeQueryValue] of Object.entries(this.$route.query)) {
+        console.log(routeQueryKey)
+        if (routeQueryKey.match('\\[(.*?)\\]')) {
+          data[routeQueryKey.split('[')[0]] = {...data[routeQueryKey.split('[')[0]], [routeQueryKey.match('\\[(.*?)\\]')[1]]: +routeQueryValue}
+        } else {
+          if (routeQueryValue) {
+            if (routeQueryValue === 'true') {
+              data[routeQueryKey] = true
+            }else {
+              data[routeQueryKey] = +routeQueryValue
             }
           }
         }
-      })
+      }
+      console.log(data)
+
+
     }
   },
 };
